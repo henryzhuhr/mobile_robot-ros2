47e696719dd7c04863a70b3a1a9e4f4cbaa93b7f
HenryZhuHR <296506195@qq.com>
2023-07-18 11:54:07 +0800

    👷 ci: vuepress + github action

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: 'ROS2 开发',
+    description: 'ROS2 开发文档',
+    base: "/ros2-development/",
+})
