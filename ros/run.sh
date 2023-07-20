785256a29217bc903c4b7f642e36e69bcde1f40a
HenryZhuHR <296506195@qq.com>
2023-07-20 23:14:05 +0800

    temp save

--- ros/run.sh
+++ ros/run.sh
@@ -4,6 +4,7 @@ pip3 install setuptools-58.2.0-py3-none-any.whl
 
 rm -rf install build log
 colcon build
+source ~/.bashrc
 source /opt/ros/humble/install/setup.bash
 source install/setup.bash
 ros2 launch py_launch py_all_nodes.launch.py
--- ros/src/py_launch/launch/py_all_nodes.launch.py
+++ ros/src/py_launch/launch/py_all_nodes.launch.py
@@ -1,8 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+import platform
+
+
 def generate_launch_description():
+    node_list = []
     video_reader = Node(package="py_video", executable="video_reader", name="t1")
     video_viewer = Node(package="py_video", executable="video_viewer", name="t1")
 
-    return LaunchDescription([video_reader,video_viewer,])
+    # node_list.append(video_reader)
+    # node_list.append(video_viewer)
+
+    # 查看是否是 Jetson Nano 平台
+    device_platform = platform.machine()
+    # if device_platform=="aarch64":
+
+    # else:
+
+
+    print("add node", node_list)
+
+    return LaunchDescription(node_list)
