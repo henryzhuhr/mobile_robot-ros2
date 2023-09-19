bad9e0fccce685a4e083a7e09532497a222b1ceb
HenryZhuHR <296506195@qq.com>
2023-09-19 12:53:05 +0800

    🐳 deploy: 新增适用于树莓派(jammy)的 docker 部署

---
+++ docker/run-aarch64-jammy.sh
@@ -0,0 +1,7 @@
+export DOCKER_COMPOSE_FILE="docker/compose/docker-compose.ros-humble-desktop.aarch64.jammy.yml"
+
+# fast run
+sudo docker compose -f ${DOCKER_COMPOSE_FILE} run ros bash 
+
+# rm
+# sudo docker-compose rm -f ${DOCKER_COMPOSE_FILE} 
